44a5fe9da56bcb8fad5454e63fb7ddd48a02bf69
Wideweb <alckevich@live.com>
2015-06-05 14:50:57 +0300

    Not all files have been fixed

---
+++ DirectXGame/TCPListener.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using TransferingDataLib;
+
+namespace DirectXGame
+{
+    class TCPListener
+    {
+        const int BUFFER_SIZE = 1024;
+
+        Socket listener;
+        byte[] buffer;
+
+        public delegate void EndPointReceivedHandler(EndPoint e);
+        public event EndPointReceivedHandler Received;
+
+        public bool Running
+        {
+            get;
+            private set;
+        }
+
+        public void Start(int port)
+        {
+            if (Running)
+                return;
+
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, port));
+            listener.Listen(0);
+
+            buffer = new byte[BUFFER_SIZE];
+            listener.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceivedCallback, null);
+            Running = true;
+        }
+
+        public void Stop()
+        {
+            if (!Running)
+                return;
+
+            listener.Close();
+            Running = false;
+        }
+
+        private void ReceivedCallback(IAsyncResult ar)
+        {
+            try
+            {
+                EndPoint remote_ep = new IPEndPoint(IPAddress.Any, 0);
+                int rec = listener.EndReceiveFrom(ar, ref remote_ep);
+                
+                if (Received != null)
+                    Received(remote_ep);
+            }
+            catch { }
+
+            if (Running)
+            {
+                try
+                {
+                    listener.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceivedCallback, null);
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ DirectXGame/UDPSpeaker.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using TransferingDataLib;
+
+namespace DirectXGame
+{
+    class UDPSpeaker
+    {
+        Socket speaker;
+        IPEndPoint ep;
+        byte[] buffer;
+
+        public bool Running { get; private set; }
+
+        public UDPSpeaker(int port)
+        {
+            ep = new IPEndPoint(IPAddress.Broadcast, port);
+        }
+
+        public void StartBroadcasting(string text)
+        {
+            if (Running)
+                return;
+
+            speaker = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+            speaker.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            speaker.Ttl = 1;
+            buffer = Encoding.ASCII.GetBytes(text);
+            speaker.BeginSendTo(buffer, 0, buffer.Length, SocketFlags.None, ep, SendToCallback, null);
+
+            Running = true;
+        }
+
+        public void Stop()
+        {
+            if (!Running)
+                return;
+
+            speaker.Close();
+            Running = false;
+        }
+
+        private void SendToCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int sendedBytes = speaker.EndSendTo(ar);
+            }
+            catch { }
+
+            if (Running)
+            {
+                try
+                {
+                    Thread.Sleep(500);
+                    speaker.BeginSendTo(buffer, 0, buffer.Length, SocketFlags.None, ep, SendToCallback, null);
+                }
+                catch { }
+            }
+        }
+
+        private IPAddress LocalIPAddress()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return null;
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            return host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+        }
+
+    }
+}
